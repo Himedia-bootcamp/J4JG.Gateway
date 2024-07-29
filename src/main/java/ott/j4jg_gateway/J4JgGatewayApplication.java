0747f438cae6e144840f8fbde23083b284546404
악덕 <rudejrqkr97@gmail.com>
2024-07-29 17:11:32 +0900

    feat: RESTful API 호출을 위한 Feign Client 활성화
    - J4JgGatewayApplication.java 파일에 Feign Client 활성화 어노테이션 추가
    - 새로운 RESTful API 호출을 위한 간단한 서비스간 통신 지원
    - 코드베이스에 쉽게 RESTful API 호출이 가능하도록 개선함

--- src/main/java/ott/j4jg_gateway/J4JgGatewayApplication.java
+++ src/main/java/ott/j4jg_gateway/J4JgGatewayApplication.java
@@ -2,8 +2,10 @@ package ott.j4jg_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients // 간단한 서비스간 통신, 선언적 방식으로 쉽게 RESTful API를 호출
 public class J4JgGatewayApplication {
 
     public static void main(String[] args) {
