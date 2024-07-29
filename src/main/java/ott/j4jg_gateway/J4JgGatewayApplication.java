ceab4fb77dd36f6d58d1bd3f03fd838ddd2c4934
악덕 <rudejrqkr97@gmail.com>
2024-07-29 16:39:16 +0900

    :tada: init

---
+++ src/main/java/ott/j4jg_gateway/J4JgGatewayApplication.java
@@ -0,0 +1,13 @@
+package ott.j4jg_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J4JgGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J4JgGatewayApplication.class, args);
+    }
+
+}
